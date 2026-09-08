741bd3f69ed8c54563acc3389b5ba35d98d0ddf2
Myo Tun Aung <myotunaung.dev@gmail.com>
2026-09-08 09:26:13 +0700

    fix: force nodemailer to use ipv4 with family 4

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -7,6 +7,7 @@ export const sendEmail = async (options) => {
             host: 'smtp.gmail.com',
             port: 587,
             secure: false,
+            family: 4,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
